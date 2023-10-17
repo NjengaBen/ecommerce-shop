35e0eeef918e8b4fe96d1a64f9c6adb70d05d370
NjengaBen <bennjenga2@gmail.com>
2023-10-18 01:58:51 +0300

    images

--- app/product/[slug]/page.jsx
+++ app/product/[slug]/page.jsx
@@ -1,20 +1,29 @@
 import Image from "next/image";
 import { client, urlFor } from "@/lib/client";
 
+export const generateStaticParams = async () => {
+  const query = `*[_type == "product"]{slug{current}}`;
+  const products = await client.fetch(query);
+  return products.map((product) => ({
+    slug: product.slug.current,
+  }));
+};
 async function getProductDetails(slug) {
   const query = `*[_type == "product" && slug.current == "${slug}"][0]`;
   const details = await client.fetch(query);
   return details;
 }
 
-async function getProducts() {
+async function getProducts(slug) {
   const query = `*[_type == "product"]`;
   const products = await client.fetch(query);
   return products;
 }
-const ProductDetails = async ({ params: { slug } }) => {
-  const details = getProductDetails(slug);
-  const products = getProducts(slug);
+const ProductDetails = async ({ params }) => {
+  const { slug } = params;
+  const details = await getProductDetails(slug);
+  const products = await getProducts(slug);
+  // console.log(details);
 
   return (
     <div>
@@ -22,7 +31,7 @@ const ProductDetails = async ({ params: { slug } }) => {
         <div>
           <div className="image-container">
             <Image
-              src={urlFor(details.image)}
+              src={urlFor(details.image && details.image[0])}
               alt="product"
               width={450}
               height={450}
