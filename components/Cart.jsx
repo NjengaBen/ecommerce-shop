7335f53eef908cc7b5563c0a5419172de36ea1cf
NjengaBen <bennjenga2@gmail.com>
2023-10-31 23:56:38 +0300

    cleanup

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -13,7 +13,6 @@ import toast from "react-hot-toast";
 import { useStateContext } from "@/context/StateContext";
 import { urlFor } from "@/lib/client";
 import getStripe from "@/lib/getStripe";
-// import { handleCheckout } from "@/utils/getData";
 
 const Cart = () => {
   const cartRef = useRef();
